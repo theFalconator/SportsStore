0a735b6a42f2464143e06667d651db0d66f52f85
theFalconator <falcon.nick@gmail.com>
2018-03-28 18:54:46 -0500

    added unit tests added unit test for pagination

---
+++ SportsStore.Tests/ProductControllerTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Moq;
+using SportsStore.Controllers;
+using SportsStore.Models;
+using Xunit;
+
+namespace SportsStore.Tests
+{
+    public class ProductControllerTests
+    {
+        [Fact]
+        public void CanPaginate()
+        {
+            // Arrange
+
+            // Create mock repository with 5 fake products
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns((new Product[]
+            {
+                new Product {ProductId = 1, Name = "p1"},
+                new Product {ProductId = 2, Name = "p2"},
+                new Product {ProductId = 3, Name = "p3"},
+                new Product {ProductId = 4, Name = "p4"},
+                new Product {ProductId = 5, Name = "p5"}
+            }).AsQueryable<Product>());
+
+            // Pass mocked repository to product controller
+            var controller = new ProductController(mock.Object);
+            controller.PageSize = 3;
+            
+            // Act
+            IEnumerable<Product> result = controller.Index(2).ViewData.Model as IEnumerable<Product>;
+            
+            // Assert
+            var prodArray = result.ToArray();
+            Assert.True(prodArray.Length == 2);
+            Assert.Equal("p4", prodArray[0].Name);
+            Assert.Equal("p5", prodArray[1].Name);
+        }
+    }
+}
