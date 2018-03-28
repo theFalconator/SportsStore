e5dc64701fa736f2b4a222284442eab342f7efd1
theFalconator <falcon.nick@gmail.com>
2018-03-28 18:40:30 -0500

    added pagination to Product Controller index action

--- SportsStore/Controllers/ProductController.cs
+++ SportsStore/Controllers/ProductController.cs
@@ -10,12 +10,18 @@ namespace SportsStore.Controllers
     public class ProductController : Controller
     {
         private IProductRepository repository;
+        public int PageSize = 4;
 
         public ProductController(IProductRepository repository)
         {
             this.repository = repository;
         }
 
-        public ViewResult Index() => View(repository.Products);
+        public ViewResult Index(int productPage=1) => 
+            View(repository
+                .Products
+                .OrderBy(p=>p.ProductId)
+                .Skip((productPage-1) * PageSize)
+                .Take(PageSize));
     }
 }
