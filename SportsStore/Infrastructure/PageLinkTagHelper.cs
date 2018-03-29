ed626eb8e5a251c33754e0ebabf4212d2c76abb1
theFalconator <falcon.nick@gmail.com>
2018-03-28 19:08:42 -0500

    added paging tag helper

---
+++ SportsStore/Infrastructure/PageLinkTagHelper.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using SportsStore.Models.ViewModels;
+
+namespace SportsStore.Infrastructure
+{
+    [HtmlTargetElement("div", Attributes = "page-model")]
+    public class PageLinkTagHelper : TagHelper
+    {
+        private IUrlHelperFactory urlHelperFactory;
+
+        public PageLinkTagHelper(IUrlHelperFactory urlHelperFactory)
+        {
+            this.urlHelperFactory = urlHelperFactory;
+        }
+
+        [ViewContext]
+        [HtmlAttributeNotBound]
+        public ViewContext ViewContext { get; set; }
+
+        public PagingInfo PageModel { get; set; }
+
+        public string PageAction { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            IUrlHelper urlHelper = urlHelperFactory.GetUrlHelper(ViewContext);
+            var result = new TagBuilder("div");
+            for (int i = 0; i < PageModel.TotalPages; i++)
+            {
+                var tag = new TagBuilder("a");
+                tag.Attributes["href"] = urlHelper.Action(PageAction, new {productPage = i});
+                tag.InnerHtml.Append(i.ToString());
+                result.InnerHtml.AppendHtml(tag);
+
+            }
+
+            output.Content.AppendHtml(result.InnerHtml);
+        }
+    }
+}
