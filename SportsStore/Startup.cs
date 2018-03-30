08f03699c25ea360fa8454a0a222d49c50045c0a
theFalconator <falcon.nick@gmail.com>
2018-03-30 15:08:24 -0500

    cleaned up routes

--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -48,13 +48,27 @@ namespace SportsStore
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
-                    name: "pagination",
-                    template: "Products/Page{productPage}",
-                    defaults: new {Controller = "Product", action = "List"});
+                    name: null,
+                    template: "{category}/Page{productPage:int}",
+                    defaults: new { Controller = "Product", action = "List" });
 
                 routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Product}/{action=List}/{id?}");
+                    name: null,
+                    template: "Page{productPage}:int",
+                    defaults: new {Controller = "Product", action = "List", productPage = 1});
+
+                routes.MapRoute(
+                    name: null,
+                    template: "{category}",
+                    defaults: new { Controller = "Product", action = "List", productPage = 1 });
+
+                routes.MapRoute(
+                    name: null,
+                    template: "",
+                    defaults: new {controller = "Product", action = "List", productPage = 1});
+
+                routes.MapRoute(
+                    name: null, template: "{controller}/{action}/{id?}");
             });
             SeedData.EnsurePopulated(app);
         }
