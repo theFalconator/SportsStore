4ada08cc3781bb7ba66e79494d8703390121be92
theFalconator <falcon.nick@gmail.com>
2018-03-31 14:32:45 -0500

    added shopping cart model and unit tests

---
+++ SportsStore.Tests/CartTests.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SportsStore.Models;
+using Xunit;
+
+namespace SportsStore.Tests
+{
+    public class CartTests
+    {
+        [Fact]
+        public void CanAddNewLines()
+        {
+            Product p1 = new Product {ProductId = 1, Name = "P1"};
+            Product p2 = new Product { ProductId = 2, Name = "P2" };
+
+            Cart target = new Cart();
+
+            target.AddItem(p1, 1);
+            target.AddItem(p2, 1);
+
+            CartLine[] results = target.Lines.ToArray();
+
+            Assert.Equal(p1, results[0].Product);
+            Assert.Equal(1, results[0].Quantity);
+
+            Assert.Equal(p2, results[1].Product);
+            Assert.Equal(1, results[1].Quantity);
+        }
+
+        [Fact]
+        public void CanUpdateQuantityForExistingLines()
+        {
+            Product p1 = new Product { ProductId = 1, Name = "P1" };
+            Product p2 = new Product { ProductId = 2, Name = "P2" };
+
+            Cart target = new Cart();
+
+            target.AddItem(p1, 1);
+            target.AddItem(p2, 1);
+            target.AddItem(p1, 10);
+
+            CartLine[] results = target.Lines.ToArray();
+
+            Assert.Equal(2, results.Length);
+            Assert.Equal(11, results[0].Quantity);
+            Assert.Equal(1, results[1].Quantity);
+        }
+
+        [Fact]
+        public void CanRemoveLine()
+        {
+            Product p1 = new Product { ProductId = 1, Name = "P1" };
+            Product p2 = new Product { ProductId = 2, Name = "P2" };
+            Product p3 = new Product { ProductId = 3, Name = "P3" };
+
+            Cart target = new Cart();
+
+            target.AddItem(p1, 1);
+            target.AddItem(p2, 3);
+            target.AddItem(p3, 5);
+            target.AddItem(p2, 1);
+
+            target.RemoveLine(p2);
+
+            Assert.Equal(0, target.Lines.Count(c => c.Product == p2));
+            Assert.Equal(2, target.Lines.Count());
+        }
+
+        [Fact]
+        public void CanCalculateCartTotal()
+        {
+            Product p1 = new Product { ProductId = 1, Name = "P1", Price = 100M };
+            Product p2 = new Product { ProductId = 2, Name = "P2", Price = 50M };
+
+            Cart target = new Cart();
+
+            target.AddItem(p1, 1);
+            target.AddItem(p2, 1);
+            target.AddItem(p1, 3);
+
+            var result = target.ComputeTotalValue();
+
+            Assert.Equal(450M, result);
+        }
+
+        [Fact]
+        public void CanClearContents()
+        {
+            Product p1 = new Product { ProductId = 1, Name = "P1", Price = 100M };
+            Product p2 = new Product { ProductId = 2, Name = "P2", Price = 50M };
+
+            Cart target = new Cart();
+
+            target.AddItem(p1, 1);
+            target.AddItem(p2, 1);
+
+
+            target.Clear();
+
+            Assert.Equal(0, target.Lines.Count());
+        }
+    }
+}
